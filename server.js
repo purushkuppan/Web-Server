bf778101b7c8eb66a7b758ec221b14926565bcad
Purush Kuppan <purush.kuppan@gmail.com>
2018-08-19 23:22:07 -0500

    Web server example test
    Web server example test

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+var app = express();
+
+app.set( 'hbs' )
+
+hbs.registerPartials(__dirname+'/views/partials')
+hbs.registerHelper('copyRight', () =>{
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('upper', (text) =>{
+    return text.toUpperCase();
+})
+app.use((req, res, next)=> {
+    var log = `${new Date().toString()}`+`${req.method}`+
+        `${req.url}`+`\n`
+    fs.appendFile('server.log', `${log}`, (err) => {
+        if(err)
+            console.log('Unable to load', err)
+    })
+
+    next()
+})
+app.get('/', (req, res)=> {
+    res.render('home.hbs', {
+        name : "Purush",
+        wrk : ['Infra', 'Infosys']
+    })
+})
+
+app.listen(5000,() => {
+    console.log('App started');
+})
